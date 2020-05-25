8fd7eeab97dca3a76af5d3563d0e0f993621843a
dizico <rodrigo.maestro98@gmail.com>
2020-05-25 15:03:19 -0300

    Implementando SessionStorage

--- n2-app-master/app/home/index.controller.js
+++ n2-app-master/app/home/index.controller.js
@@ -5,12 +5,21 @@
         .module('app')
         .controller('Home.IndexController', Controller);
 
-        function Controller(UserService) {
+    function Controller(UserService) {
         var vm = this;
         vm.user = null;
-        
+            
+        let usingSessionStorage = JSON.parse(localStorage.getItem("user"));
+        console.log(usingSessionStorage)
+             
+        if (!usingSessionStorage){
+            initUser();
+        }
+        else{
+            vm.user = usingSessionStorage
+        }
+            
         function initUser() {
-            // get current user data in the API
             UserService.GetUserId().then(function (userId) {
                 UserService.GetCurrent(userId).then(function (user) {
                         vm.user = user;
